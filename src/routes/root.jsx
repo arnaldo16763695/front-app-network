2841b0f59709fab2c345591440433f9e36db1195
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2023-05-03 10:52:49 -0400

    initial commit in branch dev

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react'
 
 function Root() {
     useEffect(() => {
-      if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-             document.body.classList.add('sb-sidenav-toggled')
-          }
+        if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+            document.body.classList.add('sb-sidenav-toggled')
+        }
     }, [])
-    
+
     const addToggled = () => {
         document.body.classList.toggle('sb-sidenav-toggled');
         localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
