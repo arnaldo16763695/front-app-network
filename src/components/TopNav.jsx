8077e9f2fb5187444c82ae0c36c7732dc1f30967
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-07-03 09:58:01 -0400

    add dashword

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -99,7 +99,7 @@ const TopNav = () => {
         className="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0"
       >
         <div className="input-group">
-          <input
+          {/* <input
             className="form-control"
             type="text"
             placeholder="Search for..."
@@ -112,7 +112,7 @@ const TopNav = () => {
             type="button"
           >
             <i className="fas fa-search" />
-          </button>
+          </button> */}
         </div>
       </form>
       {/* Navbar*/}
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,9 +1,54 @@
+import { Link } from "react-router-dom";
+
 
 const Dashboard = () => {
   return (
-    <div className="container">
-      dashboard
-    </div>
+    <>
+      <div className="card mt-5">
+      
+      
+        <div className="card-header d-flex ">
+          <h4>Dashboard</h4>
+        </div>
+      
+      
+        <div className="card-body">
+      
+          <div className="row">
+            <div class="col">
+              <div className="card bg-secondary text-white mb-4">
+                <div className="card-body">Sucursales</div>
+                <div className="card-footer d-flex align-items-center justify-content-between">
+                  <Link className="small text-white stretched-link" to={'/headquarters'}>Ver detalles</Link>
+                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div className="card bg-secondary text-white mb-4">
+                <div className="card-body">Dispositivos</div>
+                <div className="card-footer d-flex align-items-center justify-content-between">
+                  <Link className="small text-white stretched-link" to={'/devices'}>View Details</Link>
+                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div className="card bg-secondary text-white mb-4">
+                <div className="card-body">Mi perfil</div>
+                <div className="card-footer d-flex align-items-center justify-content-between">
+                  <Link className="small text-white stretched-link" to={'/profile'}>View Details</Link>
+                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+      
+        </div>
+      
+      </div>
+    </>
   );
 };
 
